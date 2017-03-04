47501d1581ae23b1f7da158a60f99ae8a143e02d
kf@hydra <kf@hydra>
2017-03-05 00:31:13 +0100

    first working log cfg

--- mytb/argparse.py
+++ mytb/argparse.py
@@ -17,17 +17,22 @@ LONG_LOG_SWITCH = "--log-config"
 SHORT_LOG_SWITCH = "-L"
 LOG_CONF_ENV_VAR = "MYTB_LOG_CONFIG"
 
-def mk_parser(description="", app_name=None):
+def mk_parser(description="", app_name=None, add_help=True):
     """ creates a parser with some default switches """
     log_config = os.environ.get(LOG_CONF_ENV_VAR)
+    #print("LOG_CONFIG = %r" % log_config)
     
-    parser = argparse.ArgumentParser(description=description)
+    parser = argparse.ArgumentParser(description=description, add_help=add_help)
     if not SHORT_LOG_SWITCH:
-        parser.add_argument('--log-config', 
+        parser.add_argument(LONG_LOG_SWITCH, 
+            default=log_config,
             help="name of config module (and params)")
     else:
-        parser.add_argument('--log-config', '-L',  
-            help="name of config module (and params)")
+        parser.add_argument(LONG_LOG_SWITCH, '-L',  
+            default=log_config,
+            help=("name of config module (and params) default=%(default)s. Can "
+                "be overridden with env var {}".format( LOG_CONF_ENV_VAR))
+            )
     
 
     if not app_name:
---
+++ mytb/importlib/__init__.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+from __future__ import absolute_import, print_function
+
+# #############################################################################
+# Copyright : (C) 2017 by Teledomic.eu All rights reserved
+#
+# Name:         mytb.importlib
+#
+# Description:  helper for locating / importing modules
+#
+# #############################################################################
+
+
+
+def module_exists(modulename):
+    """ tries to find out whether a module exists """
+    import pkgutil
+    moduleparts = modulename.split('.')
+    mod = ""
+    for part in moduleparts:
+        mod += '.' + part if mod else part
+        if pkgutil.find_loader(mod) is None:
+            return False
+    return True
+
+def import_obj(obj_str):
+    """ handles input string as module.object
+        and imports the module and returns the object
+    """
+    import importlib
+    if ':' in obj_str:
+        modname, objname = obj_str.split(':')
+    else:
+        modname, objname = obj_str.rsplit('.', 1)
+    mod = importlib.import_module(modname)
+    func = getattr(mod, objname)
+    return func
+
+
+    
+
