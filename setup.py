0cc3f34c8693228d13f14149a00a88cb5c03e36d
feenes@hydra <feenes@hydra>
2018-03-25 22:46:49 +0200

    prep for pypi

--- setup.py
+++ setup.py
@@ -14,14 +14,28 @@ install_requires = [
 ]
 
 setup(name='mytb',
-      version='0.0.3',
+      version='0.0.4',
       description='my toolbox for everyday python projects',
+      long_description="small modules and tools useful for many projects",
+      long_description_content_type="text/x-rst",
       classifiers=[
+            'Development Stgatus :: 3 - Alpha',
+            'Intendened Audience ::  Developers',
+            'Topic :: Software Development :: Modules',
+            'License :: OSI Approved :: MIT License',
+            
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
       ],
-      keywords='toolbox',
+      keywords='toolbox development',
       url='https://www.teledomic.eu',
       author='Teledomic',
       author_email='info@teledomic.eu',
+      license="MIT",
       packages=[
             'mytb', 
             'mytb.commands', 
@@ -40,7 +54,16 @@ setup(name='mytb',
             'mytb = mytb.commands:main',
             ]
       },
+      project_urls={
+        'Documentation': "https://github.com/feenes/mytb",
+        'Source': "https://github.com/feenes/mytb",
+        'SayThanks': "https://github.com/feenes",
+        'Funding': 'https://donate.pypi.org',
+        'Tracker': "https://github.com/feenes/mytb/issues",
+      },
       test_suite='nose.collector',
       install_requires=install_requires,
       tests_require=['nose', 'ddt'],
-      zip_safe = False)
+      zip_safe = False,
+      python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
+      )
