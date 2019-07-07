d496989232de3381d96978e1abc4e7436b90b9c1
feenes@hydra <feenes@hydra>
2019-07-07 23:55:58 +0200

    setup.py mods: version info + urls + clean
    - version bumped to 0.0.10
    - attempt to display version urls at pypi.org
    - unify quotes in setup.py

--- setup.py
+++ setup.py
@@ -1,63 +1,62 @@
 from __future__ import absolute_import, print_function
-
 # python std modules
 
 # third party modules
 from setuptools import setup
 
 install_requires = [
-    'future',
-    'begins',
-    'dateutils',
-    'minibelt',
-    'pyyaml',
-    'pytz',
-    'tzlocal',
+    "future",
+    "begins",
+    "dateutils",
+    "minibelt",
+    "pyyaml",
+    "pytz",
+    "tzlocal",
 ]
 
-setup(name='mytb',
-      version='0.0.9',
-      description='my toolbox for everyday python projects',
+setup(name="mytb",
+      version="0.0.10",
+      author="Teledomic",
+      author_email="info@teledomic.eu",
+      description="my toolbox for everyday python projects",
       long_description="small modules and tools useful for many projects",
       long_description_content_type="text/x-rst",
       classifiers=[
-            'Development Status :: 3 - Alpha',
+            "Development Status :: 3 - Alpha",
       ],
-      keywords='toolbox development',
-      url='https://www.teledomic.eu',
-      author='Teledomic',
-      author_email='info@teledomic.eu',
+      keywords="toolbox development",
       license="MIT",
       packages=[
-            'mytb',
-            'mytb.commands',
-            'mytb.importlib',
-            'mytb.gitlab',
-            'mytb.html',
-            'mytb.logging',
-            'mytb.logging.handlers',
+            "mytb",
+            "mytb.commands",
+            "mytb.importlib",
+            "mytb.gitlab",
+            "mytb.html",
+            "mytb.logging",
+            "mytb.logging.handlers",
 
-            # might uncomment if you want that the dist package implements
+            # might comment if you want that the dist package implements
             # no tests
-            'mytb.tests',
-            'mytb.tests.gitlab',
+            "mytb.tests",
+            "mytb.tests.gitlab",
             ],
       scripts=[],
       entry_points={
-          'console_scripts': [
-            'mytb = mytb.commands:main',
+          "console_scripts": [
+            "mytb = mytb.commands:main",
             ]
       },
       project_urls={
-        'Documentation': "https://github.com/feenes/mytb",
-        'Source': "https://github.com/feenes/mytb",
-        'SayThanks': "https://github.com/feenes",
-        'Funding': 'https://donate.pypi.org',
-        'Tracker': "https://github.com/feenes/mytb/issues",
+        "Homepage": "https://www.teledomic.eu",
+        "Documentation": "https://github.com/feenes/mytb",
+        "Source": "https://github.com/feenes/mytb",
+        "SayThanks": "https://github.com/feenes",
+        "Funding": "https://donate.pypi.org",
+        "Tracker": "https://github.com/feenes/mytb/issues",
       },
       install_requires=install_requires,
-      setup_requires=['pytest-runner'],
-      tests_require=['pytest'],
+      setup_requires=["pytest-runner"],
+      tests_require=["pytest"],
       zip_safe=False,
-      python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
+      python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4",
       )
