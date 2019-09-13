ef7ba65703288229eb13560588033d458e4aabe9
feenes@hydra <feenes@hydra>
2019-09-13 18:36:05 +0200

    fix metadata for pypi package genration.
    If making a release with this modified meta data one should see
    links like Homepage and Source.
    
    As release v0.0.11 has already been done these changes will only
    be visible for the next version.

--- setup.py
+++ setup.py
@@ -2,40 +2,26 @@ from __future__ import absolute_import, print_function
 # python std modules
 
 # third party modules
+from setuptools import find_packages
 from setuptools import setup
 
-install_requires = [
-    "future",
-    "minibelt",
-]
+long_description="""small modules and tools useful for many projects
+"""
 
 setup(name="mytb",
       version="0.0.11",
-      author="Teledomic",
-      author_email="info@teledomic.eu",
       description="my toolbox for everyday python projects",
-      long_description="small modules and tools useful for many projects",
+      long_description=long_description,
       long_description_content_type="text/x-rst",
       classifiers=[
             "Development Status :: 3 - Alpha",
       ],
       keywords="toolbox development",
+      url="https://github.com/feenes",
+      author="Teledomic",
+      author_email="info@teledomic.eu",
       license="MIT",
-      packages=[
-            "mytb",
-            "mytb.commands",
-            "mytb.importlib",
-            "mytb.gitlab",
-            "mytb.html",
-            "mytb.logging",
-            "mytb.logging.configs",
-            "mytb.logging.handlers",
-
-            # might comment if you want that the dist package implements
-            # no tests
-            "mytb.tests",
-            "mytb.tests.gitlab",
-            ],
+      packages=find_packages(),
       scripts=[],
       entry_points={
           "console_scripts": [
@@ -43,14 +29,17 @@ setup(name="mytb",
             ]
       },
       project_urls={
-        "Homepage": "https://github.com/feenes",
+        "Homepage": "https://github.com/feenes/mytb",
         "Documentation": "https://github.com/feenes/mytb",
         "Source": "https://github.com/feenes/mytb",
         "SayThanks": "https://github.com/feenes",
         "Funding": "https://donate.pypi.org",
         "Tracker": "https://github.com/feenes/mytb/issues",
       },
-      install_requires=install_requires,
+      install_requires=[
+        "future",
+        "minibelt",
+        ],
       extras_require=dict(
         minimal=[],
         all=[
@@ -68,8 +57,9 @@ setup(name="mytb",
             "pyyaml",
             ],
         ),
+      python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4",
       setup_requires=["pytest-runner"],
       tests_require=["pytest"],
       zip_safe=False,
-      python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4",
+      include_package_data=True,
       )
