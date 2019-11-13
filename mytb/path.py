c1c3c3de69a2d61939e8eacb633135e3b7d8288e
feenes@hydra <feenes@hydra>
2019-11-13 11:00:19 +0100

    robust_makedirs to create dirs without race cons

---
+++ mytb/path.py
@@ -0,0 +1,20 @@
+import os
+
+from mytb.exceptions import MyTBError
+
+
+def robust_makedirs(path):
+    """ create a directory in a robust race safe manner if not
+        already existing.
+        Good for multiprocessing / threading or cases where
+        multiple actors might create a directory
+    """
+    if not os.path.isdir(path):
+        try:
+            os.makedirs(path)
+        except Exception:
+            if not os.path.isdir(path):
+                if os.path.osfile(path):
+                    raise MyTBError("path %r is not a directory" % path)
+                else:
+                    raise
