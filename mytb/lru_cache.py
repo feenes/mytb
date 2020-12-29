36e8938dcda5b75a3dd80c9bb5aec24d94c055e2
feenes@hydra <feenes@hydra>
2020-12-30 00:13:48 +0100

    rmv some more py2 related code

--- mytb/lru_cache.py
+++ mytb/lru_cache.py
@@ -1,42 +1,6 @@
 """
-py2 / py3 compatibility helper for functools.lru_cache
+former py2 / py3 compatibility helper for functools.lru_cache
+
+exists now only to avoid breaking old projectgs using this function
 """
-import sys
-
-if sys.version_info >= (3, 2):
-    from functools import lru_cache
-else:
-    from collections import OrderedDict
-
-    UNKNOWN = object()
-
-    def mk_signature(args, kwargs):
-        sig = tuple(args), tuple(kwargs.items())
-        print("sig(%s, %s) => %s" % (args, kwargs, sig))
-        return sig
-
-    def lru_cache(maxsize=128, typed=False):
-        """ simple fallback of functools.lru_cache for python2
-        """
-
-        cache = OrderedDict()
-
-        def decorate(func):
-
-            def cached_func(*args, **kwargs):
-
-                signature = tuple(args), tuple(kwargs)
-                rslt = cache.pop(signature, UNKNOWN)
-                if rslt is UNKNOWN:
-                    rslt = func(*args, **kwargs)
-
-                cache[signature] = rslt
-
-                if len(cache) > maxsize:
-                    key = next(cache.iterkeys())
-                    cache.pop(key)
-
-                return rslt
-
-            return cached_func
-        return decorate
+from functools import lru_cache  # noqa: F401
